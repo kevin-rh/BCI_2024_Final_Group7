65af0a66ea248142786ca7adc146d63b634ec39f
Kevin Richardson Halim <86484810+kevin-rh@users.noreply.github.com>
2024-06-07 17:13:42 +0800

    Add channel label export on dataSegmentation.m

--- dataSegmentation.m
+++ dataSegmentation.m
@@ -1,5 +1,7 @@
 EEG.etc.eeglabvers = '2024.0';
 
+saveDataAsCSV = 0 % Assign 1 will format data as `.csv`, otherwise `.mat` is used.
+
 % Set file access parameter
 wkdirPath = 'C:\Users\kevin\Downloads\ds002723\';
 filePath = [wkdirPath, 'preprocessed\'];
@@ -20,11 +22,15 @@ for fileId = 1:numFile
     EEGData = EEG.data; 
     EEGEventType = [EEG.urevent.type];
     EEGEventType = cast(EEGEventType(1:size(EEGEventType,2)-1), 'int8');
-
-    save([savePath, 'X\', int2str(fileId), '.mat'],"EEGData");
-    save([savePath, 'y\', int2str(fileId), '.mat'],"EEGEventType");
-
-    % save([savePath, 'X\', fileName(1:end-4), '.mat'],"EEGData");
-    % save([savePath, 'y\', fileName(1:end-4), '.mat'],"EEGEventType");
     
+    if saveDataAsCSV == 0
+        save([savePath, 'X\', int2str(fileId), '.mat'],"EEGData");
+        save([savePath, 'y\', int2str(fileId), '.mat'],"EEGEventType");
+    else
+        pop_export(EEG,[savePath, 'X\', int2str(fileId), '.csv'],'separator',',','precision',6);
+        writematrix(EEGEventType,[savePath, 'y\', int2str(fileId), '.csv']) 
+    end
 end
+
+% Extract Channel Location's Label (list of string) as csv format
+writecell({EEG.chanlocs.labels},[savePath, 'channelData.csv'])
