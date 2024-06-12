c9bdd322c9dc481bbaae1b79a84997c969009623
Kevin Richardson Halim <86484810+kevin-rh@users.noreply.github.com>
2024-06-12 12:21:46 +0800

    Update runICAExperiment.m

--- runICAExperiment.m
+++ runICAExperiment.m
@@ -1,41 +1,40 @@
+EEG.etc.eeglabvers = '2024.0';
+
 % Initialize
 ICLabelCount = zeros(3,7);
-
 totalData = 0;
 
-% Set file access parameter
-savePath = 'C:\Users\kevin\Downloads\ds002723\preprocessed\';
+% Parameter: File IO 
+wkdirPath = 'C:\Users\kevin\Downloads\ds002723\'; % Set to the working directory you currently have.
+filePath = [wkdirPath, 'ds002723\'];
+savePath = [wkdirPath, 'preprocessed\'];
 
-filePath = 'C:\Users\kevin\Downloads\ds002723\ds002723';
 patientFolders = dir(fullfile(filePath, 'sub-*'));
 patientFolders = {patientFolders.name};
 numPatient = size(patientFolders, 2);
 
-EEG.etc.eeglabvers = '2024.0';
-
-
 for folderId = 1:numPatient
     curPath = [filePath, '\', char(patientFolders(folderId)), '\eeg'];
-    files = dir(fullfile(curPath, '*.edf'));
+    filesName = dir(fullfile(curPath, '*.edf'));
 
-    filesNames = {files.name};
+    filesNames = {filesName.name};
     numFile = size(filesNames, 2);
-    totalData = totalData + numFile;
 
     for fileId = 1:numFile
         % Load  dataset
         fileName = char(filesNames(fileId));
-        curFile = [curPath, '\', fileName];   
-        EEG = pop_biosig(curFile);       
+        curDataPath = [curPath, '\', fileName];   
+        EEG = pop_biosig(curDataPath);       
 
         % Remove non-EEG Channels
         EEG = pop_select( EEG, 'rmchannel',{'GSR','ECG','VA1','VA2'});
 
         % Set Channels Location
         EEG = pop_chanedit(EEG, 'load',{[filePath, '\channel-we-use.ced'],'filetype','autodetect'});
+        
+        % Experiment 1
         EEG = pop_runica(EEG, 'icatype', 'picard', 'maxiter',500);
         EEG = pop_iclabel(EEG, 'default');
-
         [~, rowIndices] = max(EEG.etc.ic_classification.ICLabel.classifications, [], 2);
         for rowIndId = 1:size(rowIndices)
             maxId = rowIndices(rowIndId);
@@ -49,6 +48,7 @@ for folderId = 1:numPatient
         EEG = pop_eegfiltnew(EEG, 'hicutoff',120,'plotfreqz',1);
         EEG = pop_cleanline(EEG, 'bandwidth',2,'chanlist',[1:32] ,'computepower',1,'linefreqs',[50 100] ,'newversion',0,'normSpectrum',0,'p',0.01,'pad',2,'plotfigures',0,'scanforlines',0,'sigtype','Channels','taperbandwidth',2,'tau',100,'verb',1,'winsize',4,'winstep',1);
         
+        % Experiment 2
         EEG = pop_runica(EEG, 'icatype', 'picard', 'maxiter',500);
         EEG = pop_iclabel(EEG, 'default');
         [~, rowIndices] = max(EEG.etc.ic_classification.ICLabel.classifications, [], 2);
@@ -60,6 +60,7 @@ for folderId = 1:numPatient
         % ASR-Corrected the EEG
         EEG = pop_clean_rawdata(EEG, 'FlatlineCriterion','off','ChannelCriterion','off','LineNoiseCriterion','off','Highpass','off','BurstCriterion',20,'WindowCriterion','off','BurstRejection','off','Distance','Euclidian');
         
+        % Experiment 3 & ICLabel save
         EEG = pop_runica(EEG, 'icatype', 'picard', 'maxiter',500);
         EEG = pop_iclabel(EEG, 'default');
         [~, rowIndices] = max(EEG.etc.ic_classification.ICLabel.classifications, [], 2);
@@ -67,10 +68,19 @@ for folderId = 1:numPatient
             maxId = rowIndices(rowIndId);
             ICLabelCount(3, maxId) = ICLabelCount(3, maxId) + 1;
         end
+
+        % Remove Bad Components and Reconstruct Good Components
+        EEG = pop_icflag(EEG, [NaN NaN; 0.8 1; 0.8 1; 0.8 1; 0.8 1; 0.8 1; NaN NaN]);
+        EEG = pop_subcomp( EEG, [], 0);
         
+        % Save as a MATLAB dataset (.set) format
         EEG = pop_saveset( EEG, 'filename',[fileName(1:end-4), '.set'],'filepath',savePath);
+
+        totalData = totalData + 1;
     end
 end
 
+% Display & Save Result
 avgICLabelCount = ICLabelCount/totalData;
 disp(avgICLabelCount);
+writecell({avgICLabelCount},[savePath, 'experimentResult.csv'])
